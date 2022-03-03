3a1f4059dba950bae921a01a98cdfc62c0ab6630
Yusuf Kaka <yusuf.kaka@mtn.com>
2022-03-03 17:40:40 +0200

    repos buttons

--- src/components/repos.jsx
+++ src/components/repos.jsx
@@ -16,12 +16,14 @@ export default class Repositories extends Component {
                         <div className="row">
                             <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
                                 <div className="icon-box">
-                                    <div className="icon">
-                                        <i className="bx bxl-github" />
-                                    </div>
-                                    <h4>
-                                        <a href="https://github.com/yusufk/Laser-Turret">Laser-Turret</a>
-                                    </h4>
+                                    <a href="https://github.com/yusufk/Laser-Turret">
+                                        <div className="icon">
+                                            <i className="bx bxl-github" />
+                                        </div>
+                                        <h4>
+                                            Laser-Turret
+                                        </h4>
+                                    </a>
                                     <p>
                                         Arduino and web2py code for remotely controlling a laser turret with a Leap Motion Controller
                                     </p>
@@ -29,12 +31,14 @@ export default class Repositories extends Component {
                             </div>
                             <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
                                 <div className="icon-box">
-                                    <div className="icon">
-                                        <i className="bx bxl-github" />
-                                    </div>
-                                    <h4>
-                                        <a href="https://github.com/python-smpplib/python-smpplib">Python SMPPLIB</a>
-                                    </h4>
+                                    <a href="https://github.com/python-smpplib/python-smpplib">
+                                        <div className="icon">
+                                            <i className="bx bxl-github" />
+                                        </div>
+                                        <h4>
+                                            Python SMPPLIB
+                                        </h4>
+                                    </a>
                                     <p>
                                         An SMPP Library for Python, forked and extended from an old Google Code project
                                     </p>
@@ -42,12 +46,14 @@ export default class Repositories extends Component {
                             </div>
                             <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
                                 <div className="icon-box">
-                                    <div className="icon">
-                                        <i className="bx bxl-github" />
-                                    </div>
-                                    <h4>
-                                        <a href="https://github.com/yusufk/yusufk.github.io">Github Pages resume</a>
-                                    </h4>
+                                    <a href="https://github.com/yusufk/yusufk.github.io">
+                                        <div className="icon">
+                                            <i className="bx bxl-github" />
+                                        </div>
+                                        <h4>
+                                            Github Pages resume
+                                        </h4>
+                                    </a>
                                     <p>
                                         Source code for this site, as an excercise to get familiar with React development and static websites
                                     </p>
@@ -55,12 +61,14 @@ export default class Repositories extends Component {
                             </div>
                             <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
                                 <div className="icon-box">
-                                    <div className="icon">
-                                        <i className="bx bxl-git" />
-                                    </div>
-                                    <h4>
-                                        <a href="https://bitbucket.org/mitpeople/ebilal/src">eBilal</a>
-                                    </h4>
+                                    <a href="https://bitbucket.org/mitpeople/ebilal/src">
+                                        <div className="icon">
+                                            <i className="bx bxl-git" />
+                                        </div>
+                                        <h4>
+                                            eBilal
+                                        </h4>
+                                    </a>
                                     <p>
                                         A streaming online radio client for LiveMasjid deployed on Raspberry Pi
                                     </p>
@@ -68,12 +76,14 @@ export default class Repositories extends Component {
                             </div>
                             <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
                                 <div className="icon-box">
-                                    <div className="icon">
-                                        <i className="bx bxl-gitlab" />
-                                    </div>
-                                    <h4>
-                                        <a href="https://gitlab.com/yusufkaka/tldr">Ayoba TLDR</a>
-                                    </h4>
+                                    <a href="https://gitlab.com/yusufkaka/tldr">
+                                        <div className="icon">
+                                            <i className="bx bxl-gitlab" />
+                                        </div>
+                                        <h4>
+                                            Ayoba TLDR
+                                        </h4>
+                                    </a>
                                     <p>
                                         Too Long, Didn't Read. The microapp for people with low attention spans.
                                     </p>
@@ -81,12 +91,14 @@ export default class Repositories extends Component {
                             </div>
                             <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
                                 <div className="icon-box">
-                                    <div className="icon">
-                                        <i className="bx bxl-github" />
-                                    </div>
-                                    <h4>
-                                        <a href="https://github.com/yusufk/ayoba-microapp-boilerplate">Ayoba microapp boilerplate</a>
-                                    </h4>
+                                    <a href="https://github.com/yusufk/ayoba-microapp-boilerplate">
+                                        <div className="icon">
+                                            <i className="bx bxl-github" />
+                                        </div>
+                                        <h4>
+                                            Ayoba microapp boilerplate
+                                        </h4>
+                                    </a>
                                     <p>
                                         This project demonstrates the basic workflow and lifecyle of a microapp. It waits for an onNicknameChanged event to signal that the Ayoba object is ready to be called.
                                     </p>
