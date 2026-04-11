5498565acdcaea16e3d28b092f09fe17478f3cee
Yusuf Kaka <yusufk@gmail.com>
2026-04-11 19:25:11 +0200

    Migrate from CRA to Vite - 27 vulns down to 1, 1523 packages down to 280

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-//import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'boxicons/css/boxicons.css';
@@ -9,15 +8,12 @@ import 'remixicon/fonts/remixicon.css';
 import 'swiper/css/bundle';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Navtools from './lib/navtools.js';
 import ReactGA from 'react-ga4';
 
 ReactGA.initialize('G-J0X6EB8JL4');
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
@@ -25,8 +21,3 @@ root.render(
 );
 
 Navtools();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
