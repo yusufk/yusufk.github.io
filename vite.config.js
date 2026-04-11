1991ebbaecf895006a6453da972c0cc7bfe21c0c
Yusuf Kaka <yusufk@gmail.com>
2026-04-11 22:32:36 +0200

    Fix: add resolve.extensions for .jsx imports

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
   server: {
     port: 3000,
     open: true,
